4984bed25b686862746e37719a1e615664ac6ec9
Vanja Đenadija <130909026+vanja-djenadija@users.noreply.github.com>
2025-03-21 08:11:15 +0100

    01. Init 3 projects
    Add Web dependency

---
+++ incident-service/src/main/java/eu/reportincident/incident_service/IncidentServiceApplication.java
@@ -0,0 +1,13 @@
+package eu.reportincident.incident_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentServiceApplication.class, args);
+	}
+
+}
